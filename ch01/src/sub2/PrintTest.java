956291ce8a1f88fea2ef073c3057d23b1bba70c1
whddls0323 <whddls0323@naver.com>
2025-07-10 16:45:39 +0900

    add ch01

---
+++ ch01/src/sub2/PrintTest.java
@@ -0,0 +1,26 @@
+package sub2;
+
+/*
+날짜: 2025/07/07
+	이름: 윤종인
+	내용: Java 다양한 출력 실습
+*/
+
+public class PrintTest {
+	public static void main(String[] args) {
+		
+		// 기본 출력
+		System.out.println("Hello Java"); // 한줄복사: Ctrl + Alt + 방향키 아래
+		System.out.println("Hello Korea"); // 한줄삭제: Ctrl + d
+		System.out.println("Hello Busan"); // 작업취소: Ctrl + z
+		
+		// 한줄출력
+		System.out.print("Welcome"); //ln(줄바꿈, 개행) 처리가 안되기 때문에 그 다음 출력문이 바로 이어서 출력됨.
+		System.out.print("Java!");
+		
+		// 개행 출력
+		//System.out.println(); // 줄바꿈
+		System.out.print("Welcome\n"); // \n: 개행처리 이스케이프 특수문자
+		System.out.print("Korea!");
+	}
+}
