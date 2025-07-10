751fb01d267310fd22e173636264fcf5af57ed86
whddls0323 <whddls0323@naver.com>
2025-07-10 16:48:49 +0900

    add DataStructure

---
+++ DataStructure/src/sub3/SelectionSortTest.java
@@ -0,0 +1,33 @@
+package sub3;
+
+import java.util.Arrays;
+
+/*
+	날짜: 2025/07/08
+	이름: 윤종인
+	내용: 선택 정렬 알고리즘 실습하기
+*/
+
+public class SelectionSortTest {
+	public static void main(String[] args) {
+		int arr[] = {4,2,1,5,3};
+		
+		for(int i=0;i<4;i++) {
+			for(int j=i+1;j<5;j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[j];
+					arr[j] =  arr[i];
+					arr[i] = temp;
+				} // 24153 14253 
+				//12453
+				//12354
+				//12345
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+		
+		for(int n : arr) {
+			System.out.print(n + " ");
+		}
+	}
+}
