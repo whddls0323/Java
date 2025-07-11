c9a0389294c10e55e3968f3367dde135373be271
whddls0323 <whddls0323@naver.com>
2025-07-11 17:38:17 +0900

    java project

---
+++ CodingTest/src/step5/Main11.java
@@ -0,0 +1,25 @@
+package step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+	날짜: 2025/07/11
+	이름: 윤종인
+	내용: 백준 5단계 11번 문제 - 그대로 출력하기
+*/
+
+public class Main11 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = "";
+		
+		while((s = br.readLine()) != null) {
+			System.out.println(s);
+		}
+	}
+}
