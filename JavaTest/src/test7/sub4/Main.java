a8bf8e333ff0d960c7cc2f634828a35953db5af9
윤종인 <106676282+whddls0323@users.noreply.github.com>
2025-07-26 10:28:42 +0900

    Update Main.java

--- JavaTest/src/test7/sub4/Main.java
+++ JavaTest/src/test7/sub4/Main.java
@@ -6,24 +6,24 @@ public class Main {
 	public static void main(String[] args) {
 		Board board = new Board();
 		
-		User user1 = new User("A101","김유신");
-		User user2 = new User("A102","김춘추");
-		User user3 = new User("A103","장보고");
+		User user1 = new User("A101", "김유신");
+		User user2 = new User("A102", "김춘추");
+		User user3 = new User("A103", "장보고");
 		
-		Article article1 = new Article(1,"안녕하세요.","내용1",user1);
-		Article article2 = new Article(2,"반갑습니다.","내용2",user2);
-		Article article3 = new Article(3,"감사합니다.","내용3",user3);
+		Article article1 = new Article(1, "안녕하세요.", "내용1", user1);
+		Article article2 = new Article(2, "반갑습니다.", "내용2", user2);
+		Article article3 = new Article(3, "감사합니다.", "내용3", user3);
 		
-		Comment comment1 = new Comment(101,"1번글의 101번 댓글 입니다.");
-		Comment comment2 = new Comment(102,"2번글의 102번 댓글 입니다.");
-		Comment comment3 = new Comment(103,"1번글의 103번 댓글 입니다.");
-		Comment comment4 = new Comment(104,"3번글의 104번 댓글 입니다.");
+		Comment comment1 = new Comment(101, "1번글의 101번 댓글 입니다.");
+		Comment comment2 = new Comment(102, "2번글의 102번 댓글 입니다.");
+		Comment comment3 = new Comment(103, "1번글의 103번 댓글 입니다.");
+		Comment comment4 = new Comment(104, "3번글의 104번 댓글 입니다.");
 		
 		article1.addComment(comment1);
 		article1.addComment(comment3);
-		article1.addComment(comment2);
-		article1.addComment(comment4);
-
+		article2.addComment(comment2);
+		article3.addComment(comment4);
+		
 		board.addArticle(article1);
 		board.addArticle(article2);
 		board.addArticle(article3);
@@ -42,9 +42,8 @@ public class Main {
 			System.out.println("--------------------------------------");
 			
 			List<Comment> comments = article.getComments();
-			for(Comment comment : comments) {
+			for(Comment comment : comments)
 				System.out.println("└ "+comment.getContent());
-			}
 			System.out.println("--------------------------------------");
 		}
 	}
